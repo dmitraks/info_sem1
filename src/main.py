3bb850369f27a14508c844a821b3ccca6824fd09
shivws <kotent12345@gmail.com>
2025-10-23 21:37:53 +0300

    commit #3

--- src/main.py
+++ src/main.py
@@ -10,5 +10,5 @@ def main():
         else: sleep(1)
 
 
-if __name__ != '__main__': raise('You can\'t use it as library')
+if __name__ != '__main__': raise('You can\'t use it as a library')
 else: main()
