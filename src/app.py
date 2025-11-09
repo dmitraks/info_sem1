635b104c31376331c3e3f5035b5e55009fd99113
shivws <kotent12345@gmail.com>
2025-11-09 11:20:11 +0300

    lab #4

--- src/app.py
+++ src/app.py
@@ -2,63 +2,107 @@ import sqlite3
 from time import sleep
 from entities.workers import Workers
 from entities.positions import Positions
-from entities.departments import Departments
-from entities.powers import Powers
+from entities.rights import Rights
 
 
 def main(): # учет сотрудников и должностей
     create_tables()
-    write_data()
-    # workers, positions, departments, powers = read_data()
+    # write_examples()
+    data = read_data()
+    workers:Workers = data[0]
+    positions:Positions = data[1]
+    rights:Rights = data[2]
     print('Work with database...\n')
     while True:
-        input('What to do? 1 - read all workers, 2 - add a worker, 3 - remove a worker. Input: ')
+        iterat(workers, positions, rights)
+        sleep(1)
+
+
+def iterat(workers:Workers, positions:Positions, rights:Rights) -> None:
+    match input('\nWhat will we do? 1 - get all female workers at the position, 2 - get the workers\'s rights,\n3 - get all positions with the right. Input: '):
+        case '1':
+            pos = positions.getbyname(input('Input position: '))
+            if not pos: return print('Invalid position!')
+            params = {'position': [['==', pos]], 'sex': [['==', 1]]}
+            if (res := workers.search(params)) == None: return print('Invalid input at all!')
+            if res == []: return print('Zero workers!')
+            for worker in res: print(worker.name)
+        case '2':
+            params = {'name': [['contain', input('Input worker\'s name: ')]]}
+            if (res := workers.search(params)) == None: return print('Invalid input at all!')
+            match len(res):
+                case 0: return print('Zero workers!')
+                case 1: return print(f'{res[0].name}\'s rights: {", ".join([right.description for right in res[0].position.rights] or "Zero")}')
+            # > 1
+            params = {'birthday': [['contain', input('Input worker\'s birthday: ')]]}
+            if (res := workers.search(params, res)) == None: return print('Invalid input at all!')
+            match len(res):
+                case 0: print('Zero workers!')
+                case 1: print(f'{res[0].name}\'s rights: {", ".join([right.description for right in res[0].position.rights] or "Zero")}')
+                case _: print('Too many workers!')
+        case '3':
+            right = rights.getsbydesc(input('Input right: '))
+            if not right: return print('Zero rights!')
+            if len(right) != 1: return print('Too many rights!')
+            params = {'with': [['right', right[0]]]}
+            if (res := positions.search(params)) == None: return print('Invalid input at all!')
+            if res == []: return print('Zero positions!')
+            for position in res: print(position.name)
+        case _: print('Wrong input!')
 
 
 def create_tables():
     requests = [
-                'CREATE TABLE IF NOT EXISTS Workers(uuid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, position INTEGER DEFAULT 1 REFERENCES Positions(uuid) ON DELETE SET DEFAULT, department INTEGER DEFAULT 1 REFERENCES Departments(uuid) ON DELETE SET DEFAULT, sex BLOB, name TEXT, birthday TEXT)',
-                'CREATE TABLE IF NOT EXISTS Positions(uuid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, power INTEGER, name TEXT, description TEXT)',
-                'CREATE TABLE IF NOT EXISTS Departments(uuid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, boss INTEGER DEFAULT 1 REFERENCES Workers(uuid) ON DELETE SET DEFAULT, name TEXT, description TEXT)',
-                'CREATE TABLE IF NOT EXISTS Powers(uuid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, value BLOB, name TEXT, description TEXT)'
+                'CREATE TABLE IF NOT EXISTS Workers(uuid INTEGER PRIMARY KEY, position INTEGER DEFAULT 1 REFERENCES Positions(uuid), sex BOOLEAN, name TEXT, birthday TEXT)',
+                'CREATE TABLE IF NOT EXISTS Positions(uuid INTEGER PRIMARY KEY, name TEXT, description TEXT)',
+                'CREATE TABLE IF NOT EXISTS Rights(uuid INTEGER PRIMARY KEY, name TEXT, description TEXT)',
+                'CREATE TABLE IF NOT EXISTS PositionsToRights(uuid INTEGER PRIMARY KEY, position INTEGER REFERENCES Positions(uuid), right INTEGER REFERENCES Rights(uuid))'
     ]
     with sqlite3.connect('database.db') as con:
         cur = con.cursor()
         for request in requests: cur.execute(request)
 
 
-def write_data():
+def write_examples():
     with sqlite3.connect('database.db') as con:
         cur = con.cursor()
-        cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(1, 1, 0, "", "")')
-        cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(2, 2, 0, "Иван Иванович Иванов", "01.01.1995")')
-        cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(3, 3, 0, "Михаил Михаилович Михалков", "01.01.1990")')
-        cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(4, 4, 1, "Мария Ивановна Иванова", "01.01.2000")')
-        cur.execute('INSERT INTO positions(power, name, description) VALUES(0, "", "")')
-        cur.execute('INSERT INTO positions(power, name, description) VALUES(7, "Директор", "Управляет всей компанией")')
-        cur.execute('INSERT INTO positions(power, name, description) VALUES(7, "Менеджер персонала", "Главный HR, нанимает и увольняет")')
-        cur.execute('INSERT INTO positions(power, name, description) VALUES(4, "Генеральный секретарь", "Создает финансовые отчеты")')
-        cur.execute('INSERT INTO departments(boss, name, description) VALUES(1, "", "")')
-        cur.execute('INSERT INTO departments(boss, name, description) VALUES(2, "Руководство", "Отдел, управляющий остальными")')
-        cur.execute('INSERT INTO departments(boss, name, description) VALUES(3, "HR-отдел", "Вопросы найма и увольнения персонала")')
-        cur.execute('INSERT INTO departments(boss, name, description) VALUES(4, "Секретариат", "Финансовая отчетность компании")')
-        cur.execute('INSERT INTO powers(name, description) VALUES("kick_join", "Увольнение и найм сотрудника")')
-        cur.execute('INSERT INTO powers(name, description) VALUES("edit_workers_pos", "Изменение должности сотрудника")')
-        cur.execute('INSERT INTO powers(name, description) VALUES("view_every", "Просмотреть все данные")')
+        cur.execute('INSERT INTO workers(position, sex, name, birthday) VALUES(1, 0, "Иван Михаилович Иванов", "28.01.1975")')
+        cur.execute('INSERT INTO workers(position, sex, name, birthday) VALUES(2, 0, "Иван Иванович Иванов", "12.08.1995")')
+        cur.execute('INSERT INTO workers(position, sex, name, birthday) VALUES(3, 0, "Андрей Иванович Иванов", "17.05.1998")')
+        cur.execute('INSERT INTO workers(position, sex, name, birthday) VALUES(3, 1, "Мария Ивановна Иванова", "08.07.2000")')
+        cur.execute('INSERT INTO workers(position, sex, name, birthday) VALUES(4, 1, "Полина Павловна Павлова", "05.02.2005")')
+        cur.execute('INSERT INTO positions(name, description) VALUES("Директор", "Управляет всей компанией")')
+        cur.execute('INSERT INTO positions(name, description) VALUES("Менеджер персонала", "Главный HR, нанимает и увольняет")')
+        cur.execute('INSERT INTO positions(name, description) VALUES("Генеральный секретарь", "Создает финансовые отчеты")')
+        cur.execute('INSERT INTO positions(name, description) VALUES("Стажер", "На испытательном сроке")')
+        cur.execute('INSERT INTO rights(name, description) VALUES("kick_join", "Увольнение и найм сотрудника")')
+        cur.execute('INSERT INTO rights(name, description) VALUES("edit_workers_pos", "Изменение должности сотрудника")')
+        cur.execute('INSERT INTO rights(name, description) VALUES("view_every", "Просмотреть все данные")')
+        cur.execute('INSERT INTO rights(name, description) VALUES("view_own", "Просмотреть свои данные")')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(1, 1)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(1, 2)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(1, 3)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(1, 4)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(2, 1)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(2, 2)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(2, 3)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(2, 4)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(3, 3)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(3, 4)')
+        cur.execute('INSERT INTO positionstorights(position, right) VALUES(4, 4)')
 
 
-def read_data():
+def read_data() -> list:
     with sqlite3.connect('database.db') as con:
         cur = con.cursor()
         tworkers = cur.execute('SELECT * FROM Workers').fetchall()
         tpositions = cur.execute('SELECT * FROM Positions').fetchall()
-        tdepartments = cur.execute('SELECT * FROM Departments').fetchall()
-        tpowers = cur.execute('SELECT * FROM Powers').fetchall()
-    powers = Powers([tpowers[i][1] for i in range(len(tpowers))], [tpowers[i][2] for i in range(len(tpowers))])
-    departments = Departments(tdepartments)
-    positions = Positions(tpositions, powers)
-    workers = Workers(tworkers, positions, departments)
-    return [workers, positions, departments, powers]
+        trights = cur.execute('SELECT * FROM Rights').fetchall()
+        tpositionstorights = cur.execute('SELECT * FROM PositionsToRights').fetchall()
+    rights = Rights(trights, tpositionstorights)
+    positions = Positions(tpositions, rights, Workers)
+    workers = Workers(tworkers, positions)
+    return [workers, positions, rights]
 
 
 
--- src/entities/workers.py
+++ src/entities/workers.py
@@ -1,56 +1,81 @@
+import importlib
 import sqlite3
-from entities.departments import Department, Departments
-from entities.positions import Position, Positions
 
 
 class Worker:
     uuid: int
-    position: Position
-    department: Department
+    # position: Position
     sex: bool
     name: str
     birthday: str
 
-    def __init__(self, uuid:int, position:Position, department:Department, sex:bool, name:str, birthday:str) -> None:
+    def __init__(self, uuid:int, position, sex:bool, name:str, birthday:str) -> None:
         """ sex = 0 = male """
         self.uuid = uuid
         self.position = position
-        self.department = department
         self.sex = sex
         self.name = name
         self.birthday = birthday
+    
+    def __str__(self) -> str:
+        return f'<Worker #{self.uuid}: {self.name}>'
 
 
 class Workers:
     workers: list[Worker]
-    positions: Positions
-    departments: Departments
+    # positions: Positions
 
-    def __init__(self, data:list, positions:Positions, departments:Departments) -> None:
+    def __init__(self, data:list, positions) -> None:
         self.positions = positions
-        self.departments = departments
-        for el in data: self.workers.append(Worker(el[0], el[1], el[2], el[3], el[4], el[5]))
+        self.workers = []
+        for el in data: self.workers.append(Worker(el[0], positions.get(el[1]), el[2], el[3], el[4]))
 
     def get(self, uuid:int) -> Worker:
-        if uuid == 1: return None # type: ignore
         try: return self.workers[uuid-1]
         except IndexError: return None # type: ignore
+
+    def search(self, params:dict[str, list[list[str]]]={}, data:list[Worker]=[]) -> list[Worker]:
+        if data == []: data = self.workers
+        searched = data.copy()
+        params_all = ('uuid', 'position', 'sex', 'name', 'birthday')
+        actions = (('==', '>', '<'), ('==',), ('==',), ('==', 'contain'), ('==', 'contain'))
+        edited = False
+        for param in params:
+            if not param in params_all: return None # type: ignore
+            i = params_all.index(param)
+            for curparam in params[param]:
+                if (not curparam[0] in actions[i]) or (curparam[1] == ''): continue
+                edited = True
+                for row in searched.copy():
+                    match curparam[0]:
+                        case '==':
+                            if eval(f'row.{param}') != curparam[1]: searched.remove(row)
+                        case '<':
+                            if eval(f'row.{param}') >= curparam[1]: searched.remove(row)
+                        case '>':
+                            if eval(f'row.{param}') <= curparam[1]: searched.remove(row)
+                        case _: # 'contain'
+                            if not curparam[1].lower() in eval(f'row.{param}').lower(): searched.remove(row)
+        if len(params) != 0 and not edited:
+            print(2)
+            return None # type: ignore
+        return searched
 
     def add(self, position:int, department:int, sex:bool, name:str, birthday:str) -> int:
         if not (isinstance(position, int) and isinstance(department, int) and isinstance(sex, bool) and isinstance(name, str) and isinstance(birthday, str)): return 1
         if not self.positions.get(position): return 2
-        if not self.departments.get(department): return 3
         with sqlite3.connect('database.db') as con:
             cur = con.cursor()
-            cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(?, ?, ?, ?, ?)', (position, department, sex, name, birthday))
-        self.workers.append(Worker(len(self.workers), self.positions.get(position), self.departments.get(department), sex, name, birthday))
+            try: cur.execute('INSERT INTO workers(position, department, sex, name, birthday) VALUES(?, ?, ?, ?, ?)', (position, department, sex, name, birthday))
+            except BaseException: return -1
+        self.workers.append(Worker(len(self.workers), self.positions.get(position), sex, name, birthday))
         return 0
 
     def edit_pos(self, uuid:int, new_pos:int, uuid_from:int) -> int:
         if not (isinstance(uuid, int) and isinstance(new_pos, int) and isinstance(uuid_from, int)): return 1
         if not self.get(uuid): return 2
         if not self.get(uuid_from): return 3
-        if not self.get(uuid_from).position.checkpower('edit_worker_pos'): return 4
+        if self.get(uuid).position.anticheckright('edit_worker_pos'): return 4
         if not self.positions.get(new_pos): return 5
         with sqlite3.connect('database.db') as con:
             cur = con.cursor()
@@ -62,7 +87,7 @@ class Workers:
         if not (isinstance(uuid, int) and isinstance(uuid_from, int)): return 1
         if not self.get(uuid): return 2
         if not self.get(uuid_from): return 3
-        if not self.get(uuid_from).position.checkpower('kick_join'): return 4
+        if self.get(uuid_from).position.anticheckright('kick_join') is int: return 4
         with sqlite3.connect('database.db') as con:
             cur = con.cursor()
             cur.execute('DELETE FROM workers WHERE uuid = ?', (uuid,))
